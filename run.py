937990f1b37f703193c8b2e1706ec4782ace548b
Errucha Poke:- <58897616+jepluk@users.noreply.github.com>
2023-10-16 13:59:16 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,35 +1,54 @@
-import requests, socket
-from bs4 import BeautifulSoup as bs
+import sys, re, socket, json, urllib3
 
-p = '\x1b[38;5;255m'; h = '\x1b[38;5;46m'; m = '\x1b[38;5;160m'; o = '\x1b[38;5;220m'
-def subdomain(url, base='https://rapiddns.io/', block=[]):
-    #url = url.replace('https://','').replace('www.','').replace('http://','')
-    request = bs(requests.get(base +'/subdomain/'+ url).text, 'html.parser')
-    page = request.find_all('a', href=lambda x: x and f'/subdomain/{url}?page=' in x)
-    
-    for page_ in page:
-        patch = page_['href']
-        web = bs(requests.get(base + patch).text, 'html.parser')
+req = lambda url, **kwargs: urllib3.PoolManager(timeout=int(sys.argv[2])).request('GET', url, **kwargs)
 
-        for td in web.find_all('td'):
-            string = td.text
-            if string not in block:
-                if string.endswith(url): 
-                    try: ip = h + socket.gethostbyname(string)
-                    except socket.gaierror: ip = m +'none'
-    
-                    try: 
-                        server = requests.get('https://'+ string).headers.get('Server')
-                        if server is None: server = m +'undefined'
-                        else: server = h + server
-                    except requests.RequestException: server = m +'undefined'
-                
-                    print(f'{o}{string}{p}|{ip}{p}|{server}')
-                    block.append(string)
+def scan_subdomain(api="https://rapiddns.io/", block=[]):
+    pages = [f'{api}/subdomain/{sys.argv[1]}']
+    for url in pages:
+        response = req(url)
 
+        try:
+            next_url = re.search('<a href="(.*?)" class="page-link " aria-label="Next ">', response.data.decode('utf-8')).group(1)
+            pages.append(api + next_url)
+        except AttributeError:
+            pass
+
+        for subdomain in re.findall(r'<td>(.*?'+sys.argv[1]+'.*?)</td>', response.data.decode('utf-8')):
+            if subdomain in block: continue
+
+            block.append(subdomain)
+            send = sendr(subdomain)
+
+            address = addressr(subdomain)
+            status = send[0]
+            server = send[1]
+
+            print(f'{status}\t{address}\t{subdomain} ~ {server}')
+
+ 
+def addressr(subdomain):
+    try: 
+        return socket.gethostbyname(subdomain)
+    except:
+        return '!none'
+
+def sendr(subdomain):
+    try:
+        response = req(subdomain)
+        status = response.status
+
+        server = response.headers.get('server')
+        if server is None:
+            server = '!none'
+
+        return (str(status), server)
+    except:
+        return ('!none','!none')
 
 if __name__ == "__main__":
-    url = input(f'\n\n{p}Url ({m}ex: line.me{p}):{h} ')
-    print(p +'-'*60)
-    subdomain(url)
-
+    if len(sys.argv) == 3:
+        print('\n\nStatus\tAddress\t\tSubdomain ~ Server')
+        print('-'*60)
+        scan_subdomain()
+    else:
+        print('Failure. Ex: python run.py <domain> <timeout>')
